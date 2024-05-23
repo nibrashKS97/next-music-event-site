4405a57d64f3efee1b84292a91f9fb62152c9aa4
nibrashKS97 <kazisubahnibrash@gmail.com>
2024-05-23 12:55:23 +0600

    next.config.mjs fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-      domains: ["cdn.sanity.io", "via.placeholder.com"],
+        remotePatterns: [
+          {
+            protocol: 'https',
+            hostname: 'cdn.sanity.io',
+            port: '',
+            pathname: '**',
+          },
+          {
+            protocol: 'https',
+            hostname: 'via.placeholder.com',
+            port: '',
+            pathname: '**',
+          },
+        ],
     },
   };
   
